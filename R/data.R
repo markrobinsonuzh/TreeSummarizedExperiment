dfb2c61070dbc0ca4b6b487d0360567cc6b6a9e7
fionarhuang <fionarhuang@gmail.com>
2019-04-30 10:33:13 +0200

    remove treeC and treeR data

--- R/data.R
+++ R/data.R
@@ -12,26 +12,3 @@
 #'
 "tinyTree"
 
-#' A simulated phylogenetic tree with 5 tips and 4 internal nodes
-#'
-#' A random phylo object created using the function \code{\link[ape]{rtree}}
-#' @importFrom ape rtree
-#' @format A phylo object with 5 tips and 4 internal nodes:
-#' \describe{
-#'   \item{Tip labels}{t1, t2, ..., t5.}
-#' }
-#'
-#'
-"treeR"
-
-#' A simulated phylogenetic tree with 4 tips and 3 internal nodes
-#'
-#' A random phylo object created using the function \code{\link[ape]{rtree}}
-#' @importFrom ape rtree
-#' @format A phylo object with 4 tips and 3 internal nodes:
-#' \describe{
-#'   \item{Tip labels}{t1, t2, ..., t4.}
-#' }
-#'
-#'
-"treeC"
