6c2a98a6853a97339c7b7b376d98ea135236b242
fionarhuang <ruizhuRH@gmail.com>
2021-01-22 16:06:49 +0100

    add unit tests for joinNode

---
+++ tests/testthat/test_tree_joinNode.R
@@ -0,0 +1,17 @@
+context("joinode")
+
+test_that("joinNode works properly", {
+    data("tinyTree")
+    expect_join_equal <- function(node, truth){
+        expect_equal(joinNode(node = node, 
+                              tree = tinyTree,
+                              use.alias = FALSE),
+                     truth)
+    }
+    expect_join_equal(node = c('t4','t9'), c("Node_18" = 18))
+    expect_join_equal(node = c('t10','Node_18', 't8'), 
+                      c("Node_17" = 17, "t10" = 7))
+    expect_join_equal(node = c(5, 10), c("t4" = 5, "t3" = 10))
+    expect_error(joinNode(node = c('t4','t9', 't11'), tree = tinyTree,
+                           use.alias = FALSE))
+})
