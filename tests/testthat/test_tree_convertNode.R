042862b85ca57927e375c0519321dbf092a3615c
fionarhuang <ruizhuRH@gmail.com>
2021-02-05 13:52:48 +0100

    update warnings to check only nodes that are used

--- tests/testthat/test_tree_convertNode.R
+++ tests/testthat/test_tree_convertNode.R
@@ -1,11 +1,9 @@
 context("convertNode")
 
 data("tinyTree")
+newTree <- tinyTree
+newTree$node.label <- c(LETTERS[1:4], LETTERS[1:5])
 
-test_that("give errors when the provided arguments are not in correct form", {
-    expect_error(convertNode(tree = 2, node = 2))
-    expect_error(convertNode(tree = tinyTree, node = 20))
-})
 
 test_that("convertNode could return correct results", {
     expect_convertNode_equal <- function(test, truth){
@@ -15,6 +13,10 @@ test_that("convertNode could return correct results", {
     expect_convertNode_equal("Node_16", c(Node_16 = 16))
     expect_convertNode_equal(c(2, 4, 5), c("t7", "t9", "t4"))
     expect_convertNode_equal(2, "t7")
+    expect_error(convertNode(tree = 2, node = 2))
+    expect_error(convertNode(tree = tinyTree, node = 20))
+    
+    expect_warning(convertNode(tree = newTree, node = "B"))
 })
 
 
